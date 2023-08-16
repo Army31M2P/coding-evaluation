2468749c4503639072954d4219c59fa3c422bd6e
Army31M2P <scott.mercill@outlook.com>
2023-08-16 15:31:07 -0700

    Completed Hire Method added a recursive function to address the nested nature of the data structure.

--- dotnet/MyOrganization/Organization.cs
+++ dotnet/MyOrganization/Organization.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +12,12 @@ namespace MyOrganization
     {
         private Position root;
 
+        private int _counter;
+
         public Organization()
         {
             root = CreateOrganization();
+            _counter = 0;
         }
 
         protected abstract Position CreateOrganization();
@@ -27,7 +32,32 @@ namespace MyOrganization
         public Position? Hire(Name person, string title)
         {
             //your code here
-            return null;
+
+            //SRM:In a real system this would be a lookup or some other method to create an employee id. In this case used a global varaible to increment
+            _counter++;
+
+            //SRM: to account for white space 
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                title = string.Empty;
+            }
+
+            //SRM: create position with employee and counter (identifier)
+            var emp = new Employee(_counter, person);
+            var position = new Position(title, emp);
+
+
+            //SRM:Root we need to populate names to titles. this is a fragile design since titles can have typos...
+            if (emp != null)
+            {
+                SetTitleRecursive(title, root, emp);
+                return position;
+            }
+
+
+            //SRM: return.
+            return position;
+
         }
 
         override public string ToString()
@@ -44,5 +74,64 @@ namespace MyOrganization
             }
             return sb.ToString();
         }
+
+        /// <summary>
+        /// Method to set a users title that is Recursive.
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="current"></param>
+        /// <param name="emp"></param>
+        private void SetTitleRecursive(string title, Position? current, Employee emp)
+        {
+
+            if (current?.GetTitle() == title)
+            {
+                current?.SetEmployee(emp);
+                return;
+            }
+            else
+            {
+                if (current?.GetDirectReports().IsEmpty == false)
+                {
+                    foreach (Position reportUser in current.GetDirectReports())
+                    {
+                        if (reportUser.GetTitle() == title)
+                        {
+                            reportUser.SetEmployee(emp);
+                        }
+                        else
+                        {
+                            if (reportUser.GetDirectReports().IsEmpty == false)
+                            {
+                                //recursive function here. 
+                                SetTitleRecursive(title, reportUser, emp);
+
+                            }
+                            else //TODO:SRM this case may not be needed need to do some unit testing in the future to validate..
+                            {
+                                if (reportUser.GetTitle() == title)
+                                {
+                                    current.SetEmployee(emp);
+
+                                }
+                            }
+                        }
+
+
+                    }
+                }
+                else
+                {
+                    if (current?.GetTitle() == title)
+                    {
+                        current.SetEmployee(emp);
+
+                    }
+                }
+            }
+
+        }
+
+
     }
 }
